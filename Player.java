86cc49b9c19fd2fcc051a73f51044ad14e27a5ab
Tanisha Chetty <tchetty@smith.edu>
2025-05-04 22:19:45 -0400

    with comments for Player class :p

--- Player.java
+++ Player.java
@@ -12,11 +12,20 @@ public class Player extends Floor{
         this.inventory = new ArrayList <Objects>();
     }
 
+    /**
+     * Indicate whether the user has used the harmonize function to free themselves from the radiator
+     * @return  Boolean value indicating function has been used
+    */
     public boolean harmonize(){
         System.out.println("The radiator's tune and yours blend together to create a wonderful soothing melody. The radiator is pleased and releases you. Hear that? That's the sound of freedom.");
         return true;
     }
 
+    /**
+     * Allow the user to backflip and indicate if they did so in the Ball Pit room
+     * @param activeRoom the room the player is currently in
+     * @return  Boolean value indicating if the function was called from the Ball Pit
+    */
     public boolean backflip(Room activeRoom){
         if (activeRoom.getName() == "Ball pit"){
             return true;
@@ -26,6 +35,11 @@ public class Player extends Floor{
         }
     }
 
+    /**
+     * Allow the player to move North
+     * @param activeRoom the room the player is currently in
+     * @return  the room the player has moved to
+    */
     public Room goNorth(Room activeRoom){
         if (activeRoom.getNeighbor("NORTH") != null){
             activeRoom = activeRoom.getNeighbor("NORTH");
@@ -38,6 +52,11 @@ public class Player extends Floor{
         }
     }
 
+    /**
+     * Allow the player to move East
+     * @param activeRoom the room the player is currently in
+     * @return  the room the player has moved to
+    */
     public Room goEast(Room activeRoom){
         if (activeRoom.getNeighbor("EAST") != null){
             activeRoom = activeRoom.getNeighbor("EAST");
@@ -50,6 +69,11 @@ public class Player extends Floor{
         }
     }
 
+    /**
+     * Allow the player to move South
+     * @param activeRoom the room the player is currently in
+     * @return  the room the player has moved to
+    */
     public Room goSouth(Room activeRoom){
         if (activeRoom.getNeighbor("SOUTH") != null){
             activeRoom = activeRoom.getNeighbor("SOUTH");
@@ -62,6 +86,11 @@ public class Player extends Floor{
         }
     }
 
+    /**
+     * Allow the player to move West
+     * @param activeRoom the room the player is currently in
+     * @return  the room the player has moved to
+    */
     public Room goWest(Room activeRoom){
         if (activeRoom.getNeighbor("WEST") != null){
             activeRoom = activeRoom.getNeighbor("WEST");
@@ -74,10 +103,20 @@ public class Player extends Floor{
         }
     }
 
+    /**
+     * Describes the player's surroundings
+     * @param activeRoom the room the player is currently in
+     * @return  the description of the player's activeRoom
+    */
     public String lookAround(Room activeRoom){
         return activeRoom.getDescription();
     }
 
+    /**
+     * Allows the player to pee; first step to obtaining KeyTwo
+     * @param activeRoom the room the player is currently in
+     * @return  Boolean value based on whether they peed in the Bathroom
+    */
     public Boolean goPee(Room activeRoom){
         if (activeRoom.getName() == "Bathroom"){
             return true;
@@ -87,6 +126,11 @@ public class Player extends Floor{
         }
     }
 
+    /**
+     * Allows the player to flush; second step to obtaining KeyTwo
+     * @param activeRoom the room the player is currently in
+     * @return  Boolean value based on whether they flushed in the Bathroom
+    */
     public Boolean flush(Room activeRoom){
         if (activeRoom.getName() == "Bathroom"){
             return true;
@@ -96,6 +140,11 @@ public class Player extends Floor{
         }
     }
 
+    /**
+     * Allows the player to pee; final step to obtaining KeyTwo
+     * @param activeRoom the room the player is currently in
+     * @return  Boolean value based on whether they washed their hands in the Bathroom
+    */
     public Boolean washHands(Room activeRoom){
         if (activeRoom.getName() == "Bathroom"){
             return true;
@@ -104,7 +153,13 @@ public class Player extends Floor{
             return false;
         }
     }
-
+    /**
+     * Allows the player to pick up keys
+     * @param activeRoom the room the player is currently in
+     * @param foundKeyOne Boolean value indicating whether player has found KeyOne
+     * @param foundKeyTwo Boolean value indicating whether player has found KeyTwo
+     * @param foundKeyThree Boolean value indicating whether player has found KeyThree
+    */
     public void pickUp (Room activeRoom, Boolean foundKeyOne, Boolean foundKeyTwo, Boolean foundKeyThree){
         if (activeRoom.getName() == "Basement Cupboard" && foundKeyOne == true){
             if (this.inventory.contains(keyOne)){
@@ -132,6 +187,10 @@ public class Player extends Floor{
         }
     }
 
+    /**
+     * Empties the player's inventory
+     * @preturn returns Boolean based on whether the inventory was emptied
+    */
     public Boolean putDown(){
         if (this.inventory.contains(keyOne)||this.inventory.contains(keyTwo)||this.inventory.contains(keyThree)){
             System.out.println("You put down the key and you dropped everything you were carrying! Oops! Your inventory is now empty. Retrace your steps to get your items back.");
@@ -142,6 +201,10 @@ public class Player extends Floor{
         }
     }
 
+    /**
+     * Allows player to use keys in the exit door
+     * @preturn Boolean based on whether the player's inventory contains all three keys
+    */
     public Boolean useKeys(){
         if (this.inventory.contains(keyOne)&&this.inventory.contains(keyTwo)&&this.inventory.contains(keyThree)){
             return true;
